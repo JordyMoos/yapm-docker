61f8e35a9e1a485ca3bf31c6958e2015b5f1c24d
Jordy Moos <jordymoos@gmail.com>
2017-07-28 23:07:16 +0200

    Setup the docker env

---
+++ create-default-password.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "2aa8d6891a4dbbadbed38d2fc7d473a3ebf1c92f" > ./encrypted/passhash.txt
+echo "{ \"library\": \"{\\\"blob\\\":\\\"EmUh2jg2LR5fyRpX2tcmRaj1L1GjWVZzPhT80NopUDJxLafxC4dkHWeBex0LxzOYVJivBzOVojRA7Q4peqcfAv+nYiuyHtnEY6NYjzuk4tIY77ruqwda08YplRd/gg66G0XEJRv8mthRGv4QOUvGCJRwzJ2kwXwACb+Xcii3FfB9SHkR7VuExjxjMl4QAZg7\\\",\\\"library_version\\\":1,\\\"api_version\\\":3,\\\"modified\\\":1429747047}\", \"hmac\": \"qDt6luhddSFhO/bx617+ZAG7FRRH0cpkstDp98gSbWM=\" } " > ./encrypted/passwords.txt
+
+echo ""
+echo "Default password created in ./encrypted/"
+
